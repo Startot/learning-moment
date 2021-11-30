c9c4974fd62314aba233002821af1c68d79c45c2
your mom <dababyismydad2@gmail.com>
2021-11-30 09:32:24 -0600

    Lessin 4: No More Comments!

--- learning-app.swift/ViewModels/ContentModel.swift
+++ learning-app.swift/ViewModels/ContentModel.swift
@@ -21,37 +21,29 @@ class ContentModel: ObservableObject {
     
     func getLocalData() {
         
-        // Get a url to the json file
         let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
         
         do {
-            // Read the file into a data object
             let jsonData = try Data(contentsOf: jsonUrl!)
             
-            // Try to decode the json into an array of modules
             let jsonDecoder = JSONDecoder()
             let modules = try jsonDecoder.decode([Module].self, from: jsonData)
             
-            // Assign parsed modules to modules property
             self.modules = modules
         }
         catch {
-            // TODO log error
             print("Couldn't parse local data")
         }
         
-        // Parse the style data
         let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
         
         do {
             
-            // Read the file into a data object
             let styleData = try Data(contentsOf: styleUrl!)
             
             self.styleData = styleData
         }
         catch {
-            // Log error
             print("Couldn't parse style data")
         }
         
